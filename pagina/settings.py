5fe41698ed2436c4c1c04421f0c46375a29e472f
MetalFKingRawner <150486340+MetalFKingRawner@users.noreply.github.com>
2024-06-18 16:33:25 -0600

    Update settings.py

--- pagina/settings.py
+++ pagina/settings.py
@@ -143,5 +143,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'contactoinmobiliariahome@gmail.com'
-EMAIL_HOST_PASSWORD = 'yddrzyrxcogrwoax'
+EMAIL_HOST_PASSWORD = 'xrrn apzn xebb mqrq'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
