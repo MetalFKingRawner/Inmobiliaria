e49641ca68e2ae7b2b0b67f67dc3a0f02fbb6474
MetalFKingRawner <150486340+MetalFKingRawner@users.noreply.github.com>
2024-06-18 23:40:19 -0600

    Update settings.py

--- pagina/settings.py
+++ pagina/settings.py
@@ -142,6 +142,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'rawner81@gmail.com'
-EMAIL_HOST_PASSWORD = 'qkhg shrd zkre amlz'
+EMAIL_HOST_USER = 'contactoinmobiliariahome@gmail.com'
+EMAIL_HOST_PASSWORD = 'xrrnapznxebbmqrq'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
