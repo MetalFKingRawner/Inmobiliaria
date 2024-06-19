f1ce0aa51547b9d05457cc20fe6e192edfae613c
MetalFKingRawner <150486340+MetalFKingRawner@users.noreply.github.com>
2024-06-18 23:04:54 -0600

    Update settings.py

--- pagina/settings.py
+++ pagina/settings.py
@@ -143,5 +143,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'rawner81@gmail.com'
-EMAIL_HOST_PASSWORD = 'yddrzyrxcogrwoax'
+EMAIL_HOST_PASSWORD = 'qkhg shrd zkre amlz'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
